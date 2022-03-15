8bf2e2c15df023d2b2a4cea311880e4b878856c9
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-03-15 22:56:20 +0800

    hw2

---
+++ HW2/hw02_00.m
@@ -0,0 +1,25 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Assignment Number: 2
+% Problem number: Example
+% Student Name:  ...
+% Student ID: ...
+% Email address: ...
+% Department: ...
+% Date: ...
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clf; clear; clc;
+fprintf('Student ID:.. Name:..\n');
+
+x = -10:0.05:10;
+while 1
+	coef = input('Please input the coefficients [a,b]:');
+	if coef(1) == 0 
+	   fprintf('Thanks for playering.\n');
+	   break
+    end
+    a = coef(1); b = coef(2);
+	y = a .*x + b;
+	plot(x,y);
+	hold on;
+end
