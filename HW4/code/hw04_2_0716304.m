e47461669968092c125b98f44edf6c620f9bb723
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-05-13 08:40:08 +0800

    matlab hw 4

---
+++ HW4/code/hw04_2_0716304.m
@@ -0,0 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Assignment Number: HW4
+% Problem number: 2
+% Student Name:  劉子齊
+% Student ID: 0716304
+% Email address: jonathan.tzuchi.liu@gmail.com
+% Department: Computer Science, NYCU
+% Date: 2022/05/13
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% close all windows
+% clear variables, and clear screen
+close all; clf; clear; clc;
+
+disp('HW04 Problem 4.2'); 
+
+
