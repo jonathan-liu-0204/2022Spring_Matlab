3eb30adbd9fa903295f1c81c74e5173ccc47f850
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-05-13 20:52:51 +0800

    matlab lab3.1

--- Labs/Lab03_Exercises_student_image_1_hour/code/lab03_example.m
+++ Labs/Lab03_Exercises_student_image_1_hour/code/lab03_example.m
@@ -1,19 +1,18 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Assignment Number: Lab
-% Problem number: …
-% Student Name:  …
-% Student ID: …
-% Email address: …
-% Department: Computer Science, NCTU
-% Date: ….
+% Assignment Number: Lab 3
+% Problem number: 1
+% Student Name:  ¼B¤l»ô
+% Student ID: 0716304
+% Email address: jonathan.tzuchi.liu@gmail.com
+% Department: Computer Science, NYCU
+% Date: 2022/05/13
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % close all windows
 % clear variables, and clear screen
 close all; clf; clear; clc;
 
 % show Lab
-disp('Lab')  
-
+disp('Lab Problem 3.1')  
 
 I = imread('tmp.png');
 
@@ -22,6 +21,7 @@ Id = im2double(I);
 s = size(Id);
 w = s(2);
 h = s(1);
+
 Id(1:10,:, 1) = 0;
 Id(1:10,:, 2) = 1.0;
 Id(1:10,:, 3) = 0;
